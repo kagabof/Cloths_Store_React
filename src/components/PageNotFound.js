bdeeb464b5297d7b9a91d2569d37d650512ec37e
kagabof <faustinkagabo@gmail.com>
2019-08-07 13:10:05 +0200

    Create invironment for react and components
    - Added webpack.config.dev.js
    _ Added component in the app.
    _ Home, about, courses and 404 page component

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PageNotFound = () => {
+    const style = { color: "red" };
+    return (
+        <h1 style={style}>Page not found!</h1>
+    )
+}
+
+export default PageNotFound;
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <div>
+        <h2>about KF-Collection</h2>
+        <p>This is the app that will help our company store to serve well our client.</p>
+    </div>
+);
+
+export default AboutPage;
